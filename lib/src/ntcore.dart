847dba1baea1d234d12a355159c0f09b75fc7e94
CrispyBacon1999 <bacon.josh09@gmail.com>
2022-11-02 18:03:16 -0400

    Broken code, but will fix later

--- lib/src/ntcore.dart
+++ lib/src/ntcore.dart
@@ -1,10 +1,20 @@
 import 'dart:ffi';
 import 'package:dylib/dylib.dart';
+import 'package:glob/glob.dart';
+import 'dart:io';
 
 import 'package:nt/gen/ntcore.dart';
 
-const NTCORE_PATH =
-    "allwpilib/ntcoreffi/build/libs/ntcoreffi/shared/osxx86-64/release";
+Glob ntcoreGlob = Glob("allwpilib/ntcoreffi/build/**/release");
+
+String NTCORE_PATH = Directory.current
+    .listSync()
+    .where((f) => ntcoreGlob.matches(f.path))
+    .elementAt(0)
+    .path;
+
+// const NTCORE_PATH =
+//     "allwpilib/ntcoreffi/build/libs/ntcoreffi/shared/osxx86-64/release";
 
 ntcore get nt {
   return ntcore(DynamicLibrary.open(resolveDylibPath('libntcoreffi',
